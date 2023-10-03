f08b6021d181aec135205c98cf3230bd638e2dc6
Cristian <Cristian@DESKTOP-1P2A66L>
2023-10-03 17:16:37 +0200

    Segundo metodo Terminado

--- src/empleado/TipoEmpleado.java
+++ src/empleado/TipoEmpleado.java
@@ -2,5 +2,5 @@ package empleado;
 
 public enum TipoEmpleado {
 	vendedor,
-	empleado
+	encargado
 }
--- src/empleado/empleado.java
+++ src/empleado/empleado.java
@@ -2,16 +2,14 @@ package empleado;
 
 public class empleado {
 
-	private float salario;
+	private float salario = 2000;
+	private float salarioNeto = 2000;
+	private float hExtraValor = 30;
 	
 	public float calculanominabruta(TipoEmpleado tipo, float ventasMes, float horasExtra)
 	{
 		//Tipo de empleado
-		if(tipo == TipoEmpleado.vendedor)
-		{
-			salario = 2000;
-		}
-		else
+		if(tipo == TipoEmpleado.encargado)
 		{
 			salario = 2500;
 		}
@@ -25,8 +23,29 @@ public class empleado {
 			salario = salario + 200;
 		}
 		//Horas extra
-		salario = salario + 30 * horasExtra;
+		salario = salario + hExtraValor * horasExtra;
 		
+		return salario;
+	}
+	
+	float calculoNominaNeta(float nominaBruta)
+	{
+		//Sin Comisión
+		if (salario < 2100)
+		{
+			salarioNeto = salario;
+		}
+		//Comision 15%
+		else if (salario >= 2100 && salario < 2500)
+		{
+			salarioNeto = salario * (float) 0.85;
+		}
+		//Comision 18%
+		else if (salario > 2500)
+		{
+			salarioNeto = salario * (float) 0.82;
+		}
 		
+		return salarioNeto;
 	}
 }
