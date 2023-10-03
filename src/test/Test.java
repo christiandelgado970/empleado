633d5d3a73f45b2375bac29366649ce2a16b5762
Cristian <Cristian@DESKTOP-1P2A66L>
2023-10-03 17:20:40 +0200

    Añado el test

---
+++ src/test/Test.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+class Test {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@org.junit.jupiter.api.Test
+	void testCalculanominabruta() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.jupiter.api.Test
+	void testCalculoNominaNeta() {
+		fail("Not yet implemented");
+	}
+
+}
